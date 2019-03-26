77535ebbaa1e773bcd5ffe812b22e00d26fb5df6
BIRABEN-BIANCHI Hugo <birabenbianchihugo@gmail.com>
2019-03-26 12:59:50 +0100

    Exo 2 terminé

--- src/test/java/java8/ex02/Function_02_Test.java
+++ src/test/java/java8/ex02/Function_02_Test.java
@@ -17,14 +17,20 @@ public class Function_02_Test {
     //  tag::buildAccount[]
     // TODO Compléter la fonction buildAccount
     // TODO la fonction possède 2 paramètres en entrée : une personne et un solde
-    BiFunction<Person, Integer, Account> buildAccount = null;
+    BiFunction<Person, Integer, Account> buildAccount = (t,u) ->
+    {
+    	Account a = new Account();
+		a.setOwner((Person) t);
+		a.setBalance(u);
+		return a;
+    };
     //  end::buildAccount[]
 
     @Test
     public void test_build_account() throws Exception {
 
         // TODO invoquer la fonction buildAccount pour que le test soit passant
-        Account account = null;
+        Account account = this.buildAccount.apply(new Person ("John", "France", 80, "pass"), 500);
 
         assertThat(account, hasProperty("balance", is(500)));
         assertThat(account.getOwner(), hasProperty("firstname", is("John")));
